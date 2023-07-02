145c567bcc0e0e2125e228c67b7b600f4571f000
Can <63351166+fastuptime@users.noreply.github.com>
2023-07-02 17:46:52 +0300

    Add files via upload
    <3

---
+++ index.js
@@ -0,0 +1,54 @@
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Whatsapp Bot Aktif!");
+});
+
+app.post("/check", async (req, res) => {
+    const { number } = req.params;
+    if (!number || number.length < 10 || number.length > 13 || isNaN(number)) {
+        return res.json({
+            status: "error",
+            message: "Invalid number",
+            example: "905300000000",
+        });
+    }
+    const user = await client.getContactById(`${number}@c.us`);
+    const pic = await client.getProfilePicUrl(`${number}@c.us`);
+    if (!user) return res.json({ status: "error", message: "Number not found" });
+    res.json({
+      status: "success",
+      message: "Number found",
+      data: {
+          number: number,
+          name: user?.pushname,
+          isBusiness: user?.isBusiness,
+          isEnterprise: user?.isEnterprise,
+          profilePic: pic,
+      },
+    });
+});
+
+client.initialize();
+app.listen(80, () => {
+    console.log("Sistem Aktif!");
+});
